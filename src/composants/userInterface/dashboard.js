8fbebca596c1f001f6c8eff6861e9bf9a3642044
weblegend1 <145472186+weblegend1@users.noreply.github.com>
2024-10-18 15:12:10 +0000

    display_file

--- src/composants/userInterface/dashboard.js
+++ src/composants/userInterface/dashboard.js
@@ -3,7 +3,7 @@ import Navbar from './navbar.js';
 import { FaFolder } from "react-icons/fa";
 import { FaFile } from "react-icons/fa";
 import { FaHome } from "react-icons/fa";
-import { AiOutlineFileJpg, AiOutlineFilePdf} from "react-icons/ai";
+import { AiOutlineFileJpg, AiOutlineFilePdf } from "react-icons/ai";
 import { CgFileAdd } from "react-icons/cg";
 import { RiFolderAddFill } from "react-icons/ri";
 import FileUploadModal from "./fileUpload.js";
@@ -24,6 +24,7 @@ function DashboardInterface(){
  function Dashboard(){
     const [folders, setFolders] = useState([]);
     const [files, setFiles] = useState([]);
+    const [subfolders, setSubFolders] = useState([]);
     const [openFileModal, setOpenFileModal] = useState(false);
     const [loading, setLoading] = useState(true);
     const handleOpenFileModal = () => setOpenFileModal(true);
@@ -36,12 +37,17 @@ function DashboardInterface(){
     useEffect(() => {
         const getFolderFile = async() =>{
             try {
-                const folderResponse = await axios.get("http://127.0.0.1:8000/api/folders");
-                setFolders(folderResponse.data);
-                console.log(folderResponse.data, 'Folder')
+                // const folderResponse = await axios.get("http://127.0.0.1:8000/api/folders");
+                // setFolders(folderResponse.data);
+                // console.log(folderResponse.data, 'Folder')
+                let userdata = JSON.parse(localStorage.getItem('userdata'));
+            let rootfolderId = userdata.rootFolder.split('root-folder-user-')[1];
                 
+                const subFolderResponse = await axios.get("http://127.0.0.1:8000/api/folders/"+rootfolderId+"/subfolders");
+                setSubFolders(subFolderResponse.data);
+                console.log(subFolderResponse.data, 'sub Folder')
 
-                const fileResponse = await axios.get("http://127.0.0.1:8000/api/file");
+                const fileResponse = await axios.get("http://127.0.0.1:8000/api/folders/"+rootfolderId+"/files");
                 setFiles(fileResponse.data);
                 console.log(fileResponse.data, 'File');
                 setLoading(false); //indique que le chargement est terminer
@@ -53,13 +59,27 @@ function DashboardInterface(){
         getFolderFile();
     },[]);
   
+    const renderFileIcon = (filename) => {
+        const fileExtension = filename.split('.').pop().toLowerCase();
+        switch (fileExtension) {
+            case 'jpg':
+            case 'jpeg':
+                return <AiOutlineFileJpg className="file-icon" />;
+            case 'pdf':
+                return <AiOutlineFilePdf className="file-icon" />;
+            default:
+                return <FaFile className="file-icon" />;
+        }
+    };
+
     if (loading) {
         return <div>Loading...</div>; // Replace with your loading component or spinner
     }
 
+
      return(
         <div>
-            <div class="sidebar">
+            <div className="sidebar">
                 <br></br>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
@@ -68,30 +88,43 @@ function DashboardInterface(){
                 </svg>
                 </div>
                 <ul>
-                    <li><a href="/dashnoard"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                    <li><a href="/dashnoard"><i className="fa fa-dashboard"></i> Dashboard</a></li>
                     <li><a href="/"><FaHome/> Home</a></li>
                     <li><a href="/folder"><FaFolder/> Folders</a></li>
                     <li><a href="/"><FaFile/> Recent Files</a></li>
-                    <li><a href="/"><i class="fa fa-cog"></i> Shared Files</a></li>
-                    <li><a href="/"><i class="fa fa-cog"></i> Deleted Files</a></li>
+                    <li><a href="/"><i className="fa fa-cog"></i> Shared Files</a></li>
+                    <li><a href="/"><i className="fa fa-cog"></i> Deleted Files</a></li>
                 </ul>
             </div>
 
-            <div class="main-content">
-                <div class = "header-content header-dashdoard">
+            <div className="main-content">
+                <div className = "header-content header-dashdoard">
                     <input type="search" id="search-bar" placeholder="search files"/>
-                    <div class="header-button">
-                        <button  onClick={handleOpenFolderModal} type="button" class="action-faff"><RiFolderAddFill size={30}/></button>
-                        <button  onClick={handleOpenFileModal} type="button" class="action-faff"><CgFileAdd size={30}/></button>
+                    <div className="header-button">
+                        <button  onClick={handleOpenFolderModal} type="button" className="action-faff"><RiFolderAddFill size={30}/></button>
+                        <button  onClick={handleOpenFileModal} type="button" className="action-faff"><CgFileAdd size={30}/></button>
                     </div>
                 </div>
-                <div class="content content-dashboard">
-                    <AiOutlineFileJpg className="react-icon"  />
-                    <AiOutlineFilePdf className="react-icon" />
-                    <AiOutlineFileJpg className="react-icon"  />
-                    <AiOutlineFilePdf className="react-icon" />
+                <div className="content content-dashboard content-grid">
+                   {/* Display Subfolders */}
+                    {subfolders.map(subfolder => (
+                        <div className="folder-item" key={subfolder.id}>
+                            <FaFolder className="folder-icon" />
+                            <p>{subfolder.foldername}</p>
+                        </div>
+                    ))}
+
+                    {/* Display Files */}
+                    {files
+                        .map(file => (
+                            <div className="file-item" key={file.id}>
+                                <FaFile className="folder-icon"/>
+                                <p>{file.fileName}</p>
+                            </div>
+                        ))}
                 </div>
             </div>
+
             <AddFolderModal open={openFolderModal} handleClose={handleCloseFolderModal} />
             <FileUploadModal open={openFileModal} handleClose={handleCloseFileModal} />
         </div>
